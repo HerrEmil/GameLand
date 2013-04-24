16cd40c45acad60f298b8487b1c884e8d55c0e5c
Emil Andersson <ean@anthillagency.com>
2013-04-24 19:08:58 +0200

    Disable webkit touch highlighting stuff

--- scripts/game.js
+++ scripts/game.js
@@ -2,13 +2,27 @@ carny.game = (function () {
 	var dom = carny.dom,
 		$ = dom.$;
 
+	// hide the active screen (if any) and show the screen
+	// with the specified id
+	function showScreen(screenId) {
+		var activeScreen = $("#game .screen.active")[0],
+			screen = $("#" + screenId)[0];
+		if (activeScreen) {
+			dom.removeClass(activeScreen, "active");
+		}
+		// Run the screen module
+		carny.screens[screenId].run();
+
+		dom.addClass(screen, "active");
+	}
+	// Called once at start
 	function setup() {
-		// disable native touchmove behavior to prevent overscroll
+		//Disable native touch move behavior to prevent overscroll
+
 		dom.bind(document, "touchmove", function (event) {
 			event.preventDefault();
 		});
-
-		// hide the address bar on Android devices
+		// Hide the address bar on Android devices
 		if (/Android/.test(navigator.userAgent)) {
 			$("html")[0].style.height = "200%";
 			setTimeout(function () {
@@ -16,26 +30,9 @@ carny.game = (function () {
 			}, 0);
 		}
 	}
-	// Hide the active screen (if any) and show the screen
-	// with the specified ID
-	function showScreen(screenId) {
-		var activeScreen = $("#game .screen.active")[0],
-			screen = $("#" + screenId)[0];
-
-		// Hide old screen html
-		if (activeScreen) {
-			dom.removeClass(activeScreen, "active");
-		}
-
-		// Run the screen module
-		carny.screens[screenId].run();
-
-		//  Display the screen html
-		dom.addClass(screen, "active");
-	}
-	//  Expose public methods
+	// expose public methods
 	return {
-		showScreen : showScreen,
-		setup : setup
+		setup : setup,
+		showScreen : showScreen
 	};
 }());
